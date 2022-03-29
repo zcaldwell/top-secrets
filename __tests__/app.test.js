f2aa4401bf68c992d1be29c2006d8a9113b66656
zcaldwell <mintyst.john@gmail.com>
2022-03-29 15:23:45 -0700

    1st test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -10,5 +11,15 @@ describe('top-secrets routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('signs up a user', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({ username: 'watson', password: 'isadog' });
+    expect(res.body).toEqual({ id: expect.any(String), username: 'watson' });
+  });
+
+
   });
 });
