b2fac78289feb70032fd266719c95271fd071a9b
zcaldwell <mintyst.john@gmail.com>
2022-03-30 15:32:27 -0700

    test returns secrets

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
+const User = require('../lib/models/User');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -31,5 +32,43 @@ describe('top-secrets routes', () => {
       .send({ username: 'watson', password: 'isadog' });
 
     expect(res.body).toEqual({ message: 'Signed in successfully!', user });
+  });
+
+  it('Delete logs out a user', async () => {
+    const agent = request.agent(app);
+
+    await UserService.create({
+      username: 'watson',
+      password: 'isadog',
+    });
+
+    await agent
+      .post('/api/v1/users/session')
+      .send({ username: 'watson', password: 'isadog' });
+
+    const res = await agent.delete('/api/v1/users/sessions');
+
+    expect(res.body).toEqual({ message: 'Logged Out' });
+  });
+
+  it('allows logged in user to view secrets', async () => {
+    const agent = request.agent(app);
+
+    await UserService.create({
+      username: 'watson',
+      password: 'isadog',
+    });
+
+    let res = await agent.get('/api/v1/secrets');
+
+    expect(res.status).toEqual(401);
+
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ username: 'watson', password: 'isadog' });
+
+    res = await agent.get('/api/v1/secrets');
+
+    expect(res.status).toEqual(200);
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS secrets;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username TEXT NOT NULL,
     password_hash TEXT NOT NULL
 );
+
+CREATE TABLE secret (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    
+)
 
 
